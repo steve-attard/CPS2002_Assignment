0c3fb5dd9fbe647974703e5277ed6500d0b40b60
ryan-mifsud <ryanmifsud680@gmail.com>
2017-03-30 15:49:05 +0200

    Finished Calculator class

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+package main.java;
+
+public class Calculator {
+
+    public int add(int a, int b){
+
+        return a+b;
+    }
+
+    public int subtract(int a, int b){
+
+        return a-b;
+    }
+
+    public int multiply(int a, int b){
+
+        return a*b;
+
+    }
+
+    public int divide(int a, int b){
+	if(b == 0) {
+
+		return 999;
+
+	}
+	else{
+
+	        return a/b;
+	
+	}
+    }
+
+}
