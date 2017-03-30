ffaddfb84bceaa8246d54254673579e06c7fdbd0
steve-attard <steve.attard.15@um.edu.mt>
2017-03-30 15:39:59 +0200

    made changes to Calculator class

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,15 @@
+package main.java;
+
 public class Calculator {
 
     public int add(int a, int b){
 
-        return 0;
+        return a+b;
     }
 
     public int subtract(int a, int b){
 
-        return 0;
+        return a-b;
     }
 
     public int multiply(int a, int b){
