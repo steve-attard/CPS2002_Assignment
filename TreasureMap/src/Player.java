445e00754eec3300560dbf98ad972061193de197
LAPTOP-9VR4GEOT\steve <steve.attard.15@um.edu.mt>
2017-04-10 13:44:55 +0200

    Updated Player Class

--- TreasureMap/src/Player.java
+++ TreasureMap/src/Player.java
@@ -1,10 +1,44 @@
 public class Player {
 
+    public char[][] table;
     public Position position = new Position();
-    private void move(char direction){
+
+    public void setTable(int size) {
+        table = new char[size][size];
+    }
+
+    public void move(char direction){
+        if(direction == 'u'){
+            position.y += 1;
+        }else if(direction == 'd'){
+            position.y -= 1;
+        }else if(direction == 'l'){
+            position.x -= 1;
+        }else if(direction == 'r'){
+            position.x += 1;
+        }
+
+        if(!this.setPosition(position)){
+            System.out.println("Invalid input");
+
+            //undo previous move
+            if(direction == 'u'){
+                position.y -= 1;
+            }else if(direction == 'd'){
+                position.y += 1;
+            }else if(direction == 'l'){
+                position.x += 1;
+            }else if(direction == 'r'){
+                position.x -= 1;
+            }
+        }
 
     }
-    private boolean setPosition(Position p){
-        return true;
+
+    public boolean setPosition(Position p){
+        if((p.y < table.length)&&(p.y >= 0)&&(p.x < table.length)&&(p.x >= 0)) {
+            return true;
+        }
+        return false;
     }
 }
