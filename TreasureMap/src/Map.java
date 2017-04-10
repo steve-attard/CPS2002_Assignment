bc5de6648762d4810eb2be1e870bc69145fe81bd
LAPTOP-9KL8SFTD\ryanm <ryanmifsud680@gmail.com>
2017-04-10 12:46:00 +0200

    Updated Map class

--- TreasureMap/src/Map.java
+++ TreasureMap/src/Map.java
@@ -1,13 +1,45 @@
+import java.util.*;
+
 public class Map {
 
     public int size;
-    private boolean setMapSize(int x, int y){
-        return true;
-    }
-    private void generate(){
+    char[][] table = new char[size][size];
+
+    Random rand = new Random();
+
+    int random_x = rand.nextInt(size);
+    int random_y = rand.nextInt(size);
+
+    public boolean setMapSize(int x, int y){
+        if(x > 50 && x < 5){
+            return false;
+        }
+        else{
+            return true;
+        }
 
     }
-    private char getTileType(int x, int y){
-        return 'a';
+    public void generate(){
+
+        table[random_x][random_y] = 't';
+        for(int i = 0; i < 2; i++){
+            while(table[random_x][random_y]  != ' '){
+                random_x = rand.nextInt(size);
+                random_y = rand.nextInt(size);
+            }
+            table[random_x][random_y] = 'w';
+        }
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(table[i][j] != 't' && table[i][j] != 'w'){
+                    table[i][j] = 'g';
+                }
+            }
+        }
+
+    }
+    public char getTileType(int x, int y){
+        return table[x][y];
     }
 }
