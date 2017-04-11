c862fc89630acdc73dfd2f79e07adbd737eefee3
LAPTOP-9VR4GEOT\steve <steve.attard.15@um.edu.mt>
2017-04-11 13:21:06 +0200

    Trying to fix build

--- TreasureMap/src/Game.java
+++ TreasureMap/src/Game.java
@@ -1,115 +1,11 @@
-import java.util.*;
-
 public class Game {
-
     public int turns;
-
+    public Player[] players;
     public Map map = new Map();
-
-    public void startGame(){
-
-        Scanner sc = new Scanner(System.in);
-        sc.useDelimiter("\n");
-
-        int minMapSize = 0;
-        int maxMapSize = 50;
-
-        System.out.println("Welcome to Treasure Map Game");
-        System.out.println("Please Enter the number of players");
-        int playerno = sc.nextInt();
-        while(!setNumPlayers(playerno)){
-            System.out.println("Please Enter the number of players");
-            playerno = sc.nextInt();
-        }
-
-        if(playerno > 1 && playerno < 5){
-            minMapSize = 5;
-        }
-        else{
-            minMapSize = 8;
-        }
-
-        System.out.println("Please Enter the size of the map");
-        int mapsize = sc.nextInt();
-        while(mapsize > maxMapSize || mapsize < minMapSize){
-            System.out.println("Invalid map size");
-            System.out.println("Please enter a valid map size");
-            mapsize = sc.nextInt();
-        }
-
-        map.size = mapsize;
-        map.generate();
-
-        ArrayList<Player> players = new ArrayList<Player>();
-
-        for(int i = 0; i < playerno; i++){
-            Player p = new Player(mapsize);
-            players.add(p);
-        }
-
-        boolean treasureFound = false;
-        char dir;
-
-        Player currentPlayer = new Player(mapsize);
-
-        while(!treasureFound){
-            for(int i = 0; i < playerno; i++){
-                //player position before moving needs to be stored.
-                System.out.println("player "+ (i+1) +" move");
-                System.out.println("player "+ (i+1) +" position: ");
-
-                currentPlayer = players.get(i);
-                System.out.println(currentPlayer.position.x + "  "  + currentPlayer.position.y);
-
-                System.out.println("Please input a direction to move");
-                dir = sc.next().toLowerCase().charAt(0);
-                while(dir!='u' && dir!='d' && dir!='l' && dir!='r'){
-                    System.out.println("Invalid input");
-                    dir = sc.next().toLowerCase().charAt(0);
-                }
-                currentPlayer.move(dir);
-
-                System.out.println("player "+ (i+1) +" position: ");
-                System.out.println(currentPlayer.position.x + "  "  + currentPlayer.position.y);
-
-                //validating correct move
-                if(!currentPlayer.isPositionInBounds(currentPlayer.position)){
-                    System.out.println("Out of Bounds!");
-                    //undo previous move
-                    if(dir == 'u'){
-                        currentPlayer.position.y += 1;
-                    }else if(dir == 'd'){
-                        currentPlayer.position.y -= 1;
-                    }else if(dir == 'l'){
-                        currentPlayer.position.x += 1;
-                    }else if(dir == 'r'){
-                        currentPlayer.position.x -= 1;
-                    }
-                    i--;
-                }
-                else{
-                    currentPlayer.table[currentPlayer.position.x][currentPlayer.position.y] =
-                    map.table[currentPlayer.position.x][currentPlayer.position.y];
-
-                    //if player landed on water tile, reset starting position
-                    if(map.table[currentPlayer.position.x][currentPlayer.position.y] == 'w'){
-                        System.out.println("You landed on a water tile!");
-                        currentPlayer.position = currentPlayer.startingPosition;
-                    }
-                    else if(map.table[currentPlayer.position.x][currentPlayer.position.y] == 't'){
-                        System.out.println("Congratulations, you found the treasure!");
-                        treasureFound = true;
-                    }
-                    else{
-                        System.out.println("You landed on a grass tile!");
-                    }
-                    currentPlayer.printTable();
-                }
-
-            }
-        }
+    public static void main(String args[]){
 
     }
+
     public boolean setNumPlayers(int n){
         if(n<2 || n>8) {
             System.out.println("Invalid number of players!");
@@ -117,7 +13,7 @@ public class Game {
         }
         return true;
     }
-    public void generateHTMLFiles(){
+    private void generateHTMLFiles(){
 
     }
 
--- TreasureMap/src/Map.java
+++ TreasureMap/src/Map.java
@@ -1,42 +1,11 @@
-import java.util.*;
-
 public class Map {
-
     public int size;
-    char[][] table;
-
-    Random rand = new Random();
-
-
-
+    public char[][] table = new char[size][size];
     public boolean setMapSize(int x, int y){
-        if(x > 50 || x < 5){
-            return false;
-        }
-        else{
-            return true;
-        }
-
+        return true;
     }
-    public void generate(){
-
-        int random_x = rand.nextInt(size);
-        int random_y = rand.nextInt(size);
-        table = new char[size][size];
-
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                table[i][j] = 'g';
-            }
-        }
-
-        table[random_x][random_y] = 't';
-        for(int i = 0; i < 2; i++){
-            while(table[random_x][random_y]  == 't' || table[random_x][random_y] == 'w'){
-                random_x = rand.nextInt(size);
-                random_y = rand.nextInt(size);
-            }
-            table[random_x][random_y] = 'w';
-        }
+    public void generate(){}
+    public char getTileType(int x, int y){
+        return 'a';
     }
 }
